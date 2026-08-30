bf107b6a73a3ee4eda840e96f525fc7730ea091e
malik-halimeh <malikhalimeh37@gmail.com>
2026-08-31 00:04:28 +0300

    dahsboard update

--- docs/task-status/member-3.js
+++ docs/task-status/member-3.js
@@ -1,9 +1,9 @@
 window.teamTaskStatus = window.teamTaskStatus || {};
 window.teamTaskStatus["member-3"] = [
-  ["E2-T01", "not_started"],
-  ["E2-T02", "not_started"],
+  ["E2-T01", "done"],
+  ["E2-T02", "done"],
   ["E2-T03", "done"],
-  ["E2-T04", "not_started"],
+  ["E2-T04", "done"],
   ["E2-T05", "not_started"],
   ["E2-T07", "not_started"]
 ];
